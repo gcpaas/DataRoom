520de3dde258c5b1f859dabe8ffb6e82c2041124
wu.jian2 <distantmtn@gmail.com>
2023-07-01 09:26:40 +0800

    style: 修改页面左侧菜单底部样式

--- data-room-ui/packages/BigScreenMag/SideMenu.vue
+++ data-room-ui/packages/BigScreenMag/SideMenu.vue
@@ -254,7 +254,8 @@ export default {
   .side-catalog-wrap{
     // padding-top: 16px;
     width: 220px;
-    height: 100%;
+    // height: 100%;
+    margin-bottom: 16px;
     box-sizing: border-box;
     color: var(--bs-el-title);
     background-color: var(--bs-background-2);
