0270d0fef45b00f9705473d44c3a33f9d815e82c
wu.jian2 <distantmtn@gmail.com>
2023-07-11 14:37:00 +0800

    fix: 修复样式文件引入方式，修改版本号

--- data-room-ui/packages/DataSourceManagement/src/index.vue
+++ data-room-ui/packages/DataSourceManagement/src/index.vue
@@ -301,7 +301,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~packages/assets/style/bsTheme.scss';
+@import '../../assets/style/bsTheme.scss';
 
 .bs-pagination {
   ::v-deep .el-input__inner {
--- data-room-ui/packages/DataSourceManagement/src/setDatasource.vue
+++ data-room-ui/packages/DataSourceManagement/src/setDatasource.vue
@@ -467,5 +467,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~packages/assets/style/bsTheme.scss';
+@import '../../assets//style/bsTheme.scss';
 </style>
