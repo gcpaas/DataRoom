5d2c97b0f2a6b1a32435006cd01ab632fb4f2821
wu.jian2 <distantmtn@gmail.com>
2023-07-25 11:10:33 +0800

    fix: 新增分组，排序值添加默认值0，修改分页样式

--- data-room-ui/packages/BigScreenDesign/ComponentDialog/index.vue
+++ data-room-ui/packages/BigScreenDesign/ComponentDialog/index.vue
@@ -43,7 +43,6 @@
               </el-select>
               <el-button
                 size="small"
-                style="margin-right: 20px"
                 type="primary"
                 @click="reSearch"
               >
