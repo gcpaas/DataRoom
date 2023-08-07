7aac34253c5d9818bc0b518cbd345965b66a697e
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-08-07 09:28:23 +0800

    feat:http数据集注释优化

--- data-room-ui/packages/DataSetManagement/src/HttpEditForm.vue
+++ data-room-ui/packages/DataSetManagement/src/HttpEditForm.vue
@@ -435,9 +435,10 @@
                     <strong>响应脚本设置规则： 接口返回数据已经内置到参数responseString(已转为字符串)中，,如果需要处理成JSON格式推荐使用JsonSlurper类。
                       <br> 例如： <br>
                       <span style="color: red;">
-                        import com.gccloud.common.utils.JSON <br>
-                        def respone = JSON.parseObject(responseString) <br>
-                        return respone.data
+                        import groovy.json.JsonSlurper<br>
+                        def jsonSlurper = new JsonSlurper()<br>
+                        def responseMap= jsonSlurper.parseText(responseString)<br>
+                        return responseMap.data.list<br>
                       </span>
                     </strong>
                   </div>
