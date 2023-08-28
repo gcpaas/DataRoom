c8e9c2de76cb1dc7fb66ccb86f661d81bb61aec1
wangkang4 <2950778279@qq.com>
2023-08-28 10:40:15 +0800

    fix:优化sqlserver连接默认值

--- data-room-ui/packages/DataSourceManagement/src/setDatasource.vue
+++ data-room-ui/packages/DataSourceManagement/src/setDatasource.vue
@@ -339,7 +339,7 @@ export default {
         case 'com.ibm.db2.jcc.DB2Driver':
           return 'jdbc:db2://localhost:5000/db_name'
         case 'com.microsoft.sqlserver.jdbc.SQLServerDriver':
-          return 'jdbc:sqlserver://localhost:1433;databaseName=mydatabase'
+          return 'jdbc:sqlserver://localhost:1433;databaseName=db_name'
         case 'org.postgresql.Driver':
           return 'jdbc:postgresql://localhost:13308/db_name'
         case 'org.apache.hive.jdbc.HiveDriver':
