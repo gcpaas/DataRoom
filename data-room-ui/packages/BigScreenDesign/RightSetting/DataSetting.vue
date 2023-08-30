1c906ac7bfc3ee28abfbcad5eb1965f4c078ab37
wu.jian2 <distantmtn@gmail.com>
2023-08-30 10:25:06 +0800

    feat: 跑马灯组件取消组件联动

--- data-room-ui/packages/BigScreenDesign/RightSetting/DataSetting.vue
+++ data-room-ui/packages/BigScreenDesign/RightSetting/DataSetting.vue
@@ -564,7 +564,6 @@
           :config="config"
           :source-field-list="sourceFieldList"
         />
-        {{ config.type }}
         <ComponentRelation
           v-if="!['carousel','gauge','liquid','marquee'].includes(config.type)"
           :config="config"
