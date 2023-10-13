b8b5e503a6eb18413ebb9cf552acaee85776e7da
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-10-13 14:17:13 +0800

    fix:添加字体文件

---
+++ data-room-ui/packages/js/utils/fontList.js
@@ -0,0 +1,22 @@
+const fontList = [
+  {
+    label: '默认',
+    value: ''
+  },
+  {
+    label: '时钟加粗倾斜',
+    value: 'ds-digitalbold_italic'
+  },
+  {
+    label: '时钟加粗正常',
+    value: 'ds-digitalbold'
+  },
+  {
+    label: '时钟倾斜',
+    value: 'ds-digitalitalic'
+  },
+  {
+    label: '时钟正常',
+    value: 'ds-digitalnormal'
+  }]
+export default fontList
