93cd27364ffa51c08b2ca5c15df9aa8530cf0093
hong.yang <hong.yang@ustcinfo.com>
2023-10-16 11:42:49 +0800

    feat: 指标卡组件添加属性

--- DataRoom/dataroom-core/src/main/java/com/gccloud/dataroom/core/module/chart/components/ScreenIndexCardChart.java
+++ DataRoom/dataroom-core/src/main/java/com/gccloud/dataroom/core/module/chart/components/ScreenIndexCardChart.java
@@ -80,6 +80,15 @@ public class ScreenIndexCardChart extends Chart {
         @ApiModelProperty(notes = "单位字体颜色")
         private String unitColor;
 
+        @ApiModelProperty(notes = "渐变方向")
+        private String gradientDirection;
+
+        @ApiModelProperty(notes = "渐变颜色0")
+        private String gradientColor0;
+
+        @ApiModelProperty(notes = "渐变颜色1")
+        private String gradientColor1;
+
     }
 
 }
