fe7ca94c7d844cf782f602bde281e88b9f477336
zhu.yawen <zhu.yawen@ustcinfo.com>
2023-10-16 10:15:38 +0800

    fix:去除旋转和扭曲配置

--- data-room-ui/packages/Render/index.vue
+++ data-room-ui/packages/Render/index.vue
@@ -38,7 +38,6 @@
       :style="{
         zIndex: chart.z || 0,
       }"
-      :transform="`perspective(${chart.perspective > 0? (chart.perspective + 'px') : 'none' }) rotateX(${chart.rotateX == undefined ? 0 : chart.rotateX}deg) rotateY(${chart.rotateY == undefined ? 0 : chart.rotateY}deg)  rotateZ(${chart.rotateZ == undefined ? 0 : chart.rotateZ}deg) skew(${chart.skewX == undefined ? 0 : chart.skewX}deg, ${chart.skewY == undefined? 0 : chart.skewY}deg)`"
       :grid="[1,1]"
       :handles="handlesList"
       class-name-handle="bs-handle-class"
@@ -85,8 +84,8 @@ import RenderCard from './RenderCard.vue'
 import Configuration from './Configuration.vue'
 // import _ from 'lodash'
 import cloneDeep from 'lodash/cloneDeep'
-// import vdr from 'gc-vue-draggable-resizable'
-import vdr from '@gcpaas/vue-draggable-resizable-gorkys'
+import vdr from 'gc-vue-draggable-resizable'
+// import vdr from '@gcpaas/vue-draggable-resizable-gorkys'
 import 'gc-vue-draggable-resizable/dist/VueDraggableResizable.css'
 import { randomString } from '../js/utils'
 import { compile } from 'tiny-sass-compiler/dist/tiny-sass-compiler.esm-browser.prod.js'
