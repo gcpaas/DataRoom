9468f2059f275798adaf16f6adc89ac8ebd9c8ba
hong.yang <hong.yang@ustcinfo.com>
2023-12-04 13:44:04 +0800

    fix: 去除多余的日志打印

--- data-room-ui/packages/BasicComponents/FlyMap/index.vue
+++ data-room-ui/packages/BasicComponents/FlyMap/index.vue
@@ -369,7 +369,6 @@ export default {
         }
         // 到达允许下钻的层数，则不再下钻
         if (this.currentDeep >= config.customize.downLevel) return
-        console.log('mapList',this.mapList)
         const mapUrl = `${window.BS_CONFIG?.httpConfigs?.baseURL}/bigScreen/map/data/${this.mapList[this.currentDeep].id}/${params.name}`
         const map = await this.$dataRoomAxios.get(decodeURI(mapUrl), {}, false)
         // 地图不可用
