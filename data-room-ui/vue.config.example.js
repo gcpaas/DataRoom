7c8d10045a3044d68e4d2edefda538fa25471cac
liu.shiyi <liu.shiyi@ustcinfo.com>
2024-08-16 15:48:38 +0800

    fix:修复部署失败问题

--- data-room-ui/vue.config.example.js
+++ data-room-ui/vue.config.example.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   publicPath:
     process.env.VUE_APP_HISTORY === 'y' ? process.env.VUE_APP_BASE : './',
-  outputDir: 'dataRoomUi',
+  outputDir: 'bigScreen',
   assetsDir: 'static',
   lintOnSave: false,
   productionSourceMap: false,
