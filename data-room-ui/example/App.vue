1cdf36239d627500cb3b9c00bc851290a2aeacc3
liu.shiyi <liu.shiyi@ustcinfo.com>
2024-08-19 17:40:28 +0800

    fix:添加免责声明

--- data-room-ui/example/App.vue
+++ data-room-ui/example/App.vue
@@ -3,6 +3,32 @@
     <transition name="fade">
       <router-view />
     </transition>
+    <el-dialog
+      class="disclaimer-dialog"
+      title="免责声明"
+      :visible.sync="dialogVisible"
+      :close-on-press-escape="false"
+      :close-on-click-modal="false"
+      :show-close="false"
+      width="80%"
+    >
+      <span v-html="message" />
+      <span
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="out">退出</el-button>
+        <el-button
+          type="primary"
+          :disabled="time>0"
+          @click="agree"
+        >我已知晓<span
+          v-if="time"
+          style="padding-left: 4px"
+        >{{ time }}</span></el-button>
+
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -14,8 +40,58 @@ export default {
     if (loading) {
       document.body.removeChild(loading)
     }
+  },
+  data () {
+    return {
+      dialogVisible: false,
+      message: '',
+      timer: null,
+      time: 10
+
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      // 如果不是演示环境或者已经阅读过免责声明，则不显示弹窗
+      if (!window?.SITE_CONFIG?.demoEnv && sessionStorage.getItem('disclaimer') === 'read ') {
+        return
+      }
+      this.dialogVisible = true
+      // 新建是否阅读免责声明的本地存储
+      sessionStorage.setItem('disclaimer', '')
+      this.$dataRoomAxios.get('/dataroom/opensource/disclaimer').then(res => {
+        this.message = res
+        this.timer = setInterval(() => {
+          if (this.time > 0) {
+            this.time--
+          } else {
+            clearInterval(this.timer)
+          }
+        }, 1000)
+      })
+    },
+    out () {
+      window.close()
+    },
+    agree () {
+      sessionStorage.setItem('disclaimer', 'true')
+      this.dialogVisible = false
+    }
   }
 }
 </script>
 <style lang="scss">
+.disclaimer-dialog{
+ .el-dialog__body{
+    height: 500px;
+    overflow-y: auto;
+  }
+  .timer-hidden{
+    opacity: 0;
+  }
+}
+
 </style>
--- data-room-ui/public/config/index-development.js
+++ data-room-ui/public/config/index-development.js
@@ -8,7 +8,8 @@
 
 window.ENV = 'development'
 var developmentConfig = {
-  baseURL: 'http://127.0.0.1:8083/dataRoomServer',
-  fileUrlPrefix: 'http://127.0.0.1:8083/dataRoomServer' + '/static'
+  baseURL: 'http://gcpaas.gccloud.com/dataRoomServer',
+  fileUrlPrefix: 'http://gcpaas.gccloud.com/dataRoomServer' + '/static'
 }
+window.SITE_CONFIG.dataRoom = {}
 window.SITE_CONFIG.dataRoom  = configDeepMerge(window.SITE_CONFIG.dataRoom , developmentConfig)
