6d75ee7a285039a51ceb2bfed6cb46c281a4a763
zhu.yawen <1044351131@qq.com>
2025-05-14 13:59:43 +0800

    refactor(login): 优化登录页面获取账号提示的图片显示
    - 动态获取图片路径，兼容不同环境
    - 调整图片样式，使其宽度占 80%
    - 移除 alt属性值，因图片本身无具体语义

--- data-room-ui/packages/Login/index.vue
+++ data-room-ui/packages/Login/index.vue
@@ -146,14 +146,15 @@ export default {
       return e.target.value
     },
     getAccount(){
-      const imgUrl = require('./assets/QQ.png') // 先获取图片路径
+      const imgUrl = process.env.VUE_APP_HISTORY === 'y' ? process.env.VUE_APP_BASE + '/static/images/QQ.png':'/static/images/QQ.png' // 先获取图片路径
       this.$alert(`
     <div>
       <p>请加入官方交流群获取演示环境登录账号：</p>
       <div style="display: flex;justify-content: center">
        <img
         src="${imgUrl}"
-        alt="891384200"
+        style="width: 80%;"
+        alt=""
       />
        </div>
     </div>
