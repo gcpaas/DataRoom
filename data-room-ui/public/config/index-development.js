e61ed2bdb536231c45c4f3912eefcf5836595f6e
zhu.yawen <1044351131@qq.com>
2026-01-21 11:14:08 +0800

    fix:简化前端配置文件

--- data-room-ui/public/config/index-development.js
+++ data-room-ui/public/config/index-development.js
@@ -1,15 +1,6 @@
-// window.ENV = 'development'
-// var developmentConfig = {
-//   baseURL: 'http://gcpaas.gccloud.com/dataRoomServer',
-//   fileUrlPrefix: 'http://gcpaas.gccloud.com/dataRoomServer' + '/static'
-// }
-// window.SITE_CONFIG.dataRoom = {}
-// window.SITE_CONFIG.dataRoom  = configDeepMerge(window.SITE_CONFIG.dataRoom , developmentConfig)
-
 window.ENV = 'development'
 var developmentConfig = {
   baseURL: 'http://127.0.0.1:8083/dataRoomServer',
   fileUrlPrefix: 'http://127.0.0.1:8083/dataRoomServer' + '/static'
 }
-window.SITE_CONFIG.dataRoom = {}
-window.SITE_CONFIG.dataRoom  = configDeepMerge(window.SITE_CONFIG.dataRoom , developmentConfig)
+window.SITE_CONFIG.dataRoom  = configDeepMerge({} , developmentConfig)
--- data-room-ui/public/config/index-production.js
+++ data-room-ui/public/config/index-production.js
@@ -1,8 +1,7 @@
 window.ENV = 'production'
 var productionConfig = {
-  baseURL: 'http://gcpaas.gccloud.com/dataRoomServer',
-  fileUrlPrefix: 'http://gcpaas.gccloud.com/dataRoomServer' + '/static'
+  baseURL: 'http://127.0.0.1:8083/dataRoomServer',
+  fileUrlPrefix: 'http://127.0.0.1:8083/dataRoomServer' + '/static'
 }
 // 必须的
-window.SITE_CONFIG.dataRoom = {}
-window.SITE_CONFIG.dataRoom  = configDeepMerge(window.SITE_CONFIG.dataRoom , productionConfig)
+window.SITE_CONFIG.dataRoom  = configDeepMerge({} , productionConfig)
