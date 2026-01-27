8c1d93249cfec0f5f15bf3cd48a1fe74167abdfd
chen.hu <741263995@qq.com>
2026-01-27 14:45:43 +0800

    chore(dependencies): 添加 javax 注解 API 依赖并移除冗余导入
    - 在 pom.xml 中添加 javax.annotation-api 依赖
    - 移除 SuccessCommandLineRunner.java 中的 jakarta 注解导入
    - 统一使用标准 Java 注解而非 Jakarta EE 注解

--- DataRoom/dataroom-server/src/main/java/com/gccloud/dataroom/init/SuccessCommandLineRunner.java
+++ DataRoom/dataroom-server/src/main/java/com/gccloud/dataroom/init/SuccessCommandLineRunner.java
@@ -1,6 +1,5 @@
 package com.gccloud.dataroom.init;
 
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
