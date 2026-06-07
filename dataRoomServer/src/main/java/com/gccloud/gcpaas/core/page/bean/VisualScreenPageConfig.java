e03f11a076c5f3b47ae5d1a517aff94c8e96d6a5
liu.chengbiao <1537130693@qq.com>
2026-06-07 13:26:00 +0800

    feat(core): 初始化 VisualScreenPageConfig 的基础配置

--- dataRoomServer/src/main/java/com/gccloud/gcpaas/core/page/bean/VisualScreenPageConfig.java
+++ dataRoomServer/src/main/java/com/gccloud/gcpaas/core/page/bean/VisualScreenPageConfig.java
@@ -1,5 +1,7 @@
 package com.gccloud.gcpaas.core.page.bean;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import lombok.Data;
 
 @Data
@@ -7,7 +9,19 @@ public class VisualScreenPageConfig extends BasePageConfig {
 
     @Override
     public void init() {
-
+        String basicConfigJson = """
+                {
+                    "background": {
+                        "fill": "color",
+                        "color": "#0F172A",
+                        "url": "",
+                        "opacity": 1,
+                        "repeat": "no-repeat"
+                    }
+                }
+                """;
+        JSONObject basicConfig = JSON.parseObject(basicConfigJson);
+        setBasicConfig(basicConfig);
     }
 
     @Override
